9213cda36ac169ae207a9d8b0254bc55e990d49e
saityalcn <stsaityalcin@gmail.com>
2023-12-24 15:07:58 +0300

    add raw material

--- pages/rawMaterial/addRawMaterial.js
+++ pages/rawMaterial/addRawMaterial.js
@@ -1,81 +1,52 @@
-import {React, useEffect, useState, useCallback} from 'react';
+import { React, useCallback, useEffect, useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import {
-    Form,
-    Loader,
-    Message,
-    TextArea
+  Button,
+  Form,
+  Icon,
+  Loader
 } from 'semantic-ui-react';
-import MainLayout from '../layouts/layout';
+import Layout from '../layouts/layout';
 import { useRouter } from 'next/router';
-import {addWorker, assignWorkerToPosition} from '../../services/employeeService'
-import {getPositions} from '../../services/positionService'
+import { addRawMaterial } from '../../services/rawMaterialService';
 
-
-let selectedPositionId;
 let request = false;
 
-export default () => {
+const addProjectForm = () => {
   const [loading, setLoading] = useState(false);
-  const [positions, setPositions] = useState([]);
-  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
   const router = useRouter();
 
-  const sendAddRawMaterialRequest = useCallback(async (event) => {
+  const createRawMaterial = useCallback(async (event) => {
     setLoading(true)
-    const jsonObject = {
-      name: event.target.quantity.value,
-      surname: event.target.deliveryDate.value,
-    }  
-    console.log(jsonObject)
-    if(!request){
-      addWorker(jsonObject).then(res => {
-        console.log(res);
-        assignWorkerToPosition(res.data.id).then(res => {
-          if(res.data.message === "Worker is not added to project"){
-            setLoading(false);
-            setShowErrorMessage(true)
-          }
-          
-          else
-            router.push('/rawMaterial/rawMaterials');
-          
-          console.log(res)
-        });
-      }).catch(err => {
-        setLoading(false)
-        console.log(err)}
-    )}}, []);
-  
-  /*
-  if(!positions)
-    getPositions().then(res => setPositions(res.data)).catch(err => console.log(err));
-      
+    const jsonObject = JSON.stringify({
+      materialName: event.target.materialName.value
+    });
 
-  if(loading || positions == null)
-    return (<Loader active/>)
-    */
+    addRawMaterial(jsonObject).then(res => {
+      router.push("/rawMaterial/rawMaterials")
+    }).catch(err => console.log(err));
+   
+  });
 
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="semantic/dist/semantic.min.css"
-      ></link>
-      <MainLayout>
-      <Message
-      error
-      hidden={!showErrorMessage}>Çalışanın ünvanına uygun bir proje bulunamadığı için çalışana proje atanamadı.</Message>
-      <Form onSubmit={sendAddRawMaterialRequest}>
-        <Form.Select label="Hammadde" placeholder='Hammadde Seçiniz' /*options={positions.map(element => {return { key: element.id, value: element.name, text: element.name }})} onChange={(e,data) => {selectedPositionId = (data.options.find(element => (element.value === data.value)).key);}}*/ ></Form.Select>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='Miktar' type="number" placeholder='Miktar Giriniz' name="quantity"/>
-          <Form.Input fluid label='Teslim Tarihi' type="date" placeholder='' name="deliveryDate"/>
-        </Form.Group>
-        <Form.Button type='submit' primary>Ekle</Form.Button>
+    <Layout>
+      <Form onSubmit={createRawMaterial}>
+        <Form.Input name="materialName" type="text" label="Hammadde Adı" placeholder="Hammadde Adı Giriniz" />
+        <Button
+          floated="right"
+          icon
+          labelPosition="left"
+          style={{ marginBottom: '2rem' }}
+          positive
+          type="submit"
+        >
+          <Icon name="plus"></Icon>
+            Oluştur
+        </Button>
       </Form>
-      </MainLayout>
-    </div>
+    </Layout>
   );
 };
+
+export default addProjectForm;
